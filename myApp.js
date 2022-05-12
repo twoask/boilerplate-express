c870899e951a19aaee5d615a546d556f35889b38
Hai Vu <twoask@gmail.com>
2022-05-11 17:10:43 -0700

    First handler

--- myApp.js
+++ myApp.js
@@ -2,7 +2,11 @@ let express = require('express');
 let app = express();
 
 // Log something
-console.log("Hello World");
+console.log('Hello World');
+
+app.get("/", function(req, res) {
+    res.send('Response String');
+});
 
 
 
