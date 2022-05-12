c879ba506b65a912739f5531347dca4f06396f22
Hai Vu <twoask@gmail.com>
2022-05-11 22:44:11 -0700

    Updated static assets for /public path

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ let indexFile = __dirname + '/views/index.html';
 let assetPath = __dirname + '/public';
 
 // Define static assets
-app.use(express.static('public'));
+app.use('/public', express.static(assetPath));
 
 // Define handlers
 app.get("/", function(req, res) {
