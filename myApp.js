5eb996e873df73db6ec8d6ed88d9f844a5c4f7f8
Hai Vu <twoask@gmail.com>
2022-05-11 22:41:31 -0700

    Added static path

--- myApp.js
+++ myApp.js
@@ -4,13 +4,18 @@ let app = express();
 // Log something
 console.log('Hello Express');
 
-let absolutePath = __dirname + '/views/index.html';
+let indexFile = __dirname + '/views/index.html';
+let assetPath = __dirname + '/public';
 
+// Define static assets
+app.use(express.static('public'));
+
+// Define handlers
 app.get("/", function(req, res) {
     // res.send('Hello Express');
-    console.log('Sending back ' + absolutePath);
+    console.log('Sending back ' + indexFile);
     // Send the HTML file as response
-    res.sendFile(absolutePath);
+    res.sendFile(indexFile);
 });
 
 
