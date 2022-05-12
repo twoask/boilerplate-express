8b27f61dabce5bc278ba77118baf9f4ef81317c5
Hai Vu <twoask@gmail.com>
2022-05-11 17:18:14 -0700

    Send file as response

--- myApp.js
+++ myApp.js
@@ -4,8 +4,13 @@ let app = express();
 // Log something
 console.log('Hello Express');
 
+let absolutePath = __dirname + '/views/index.html';
+
 app.get("/", function(req, res) {
-    res.send('Hello Express');
+    // res.send('Hello Express');
+    console.log('Sending back ' + absolutePath);
+    // Send the HTML file as response
+    res.sendFile(absolutePath);
 });
 
 
